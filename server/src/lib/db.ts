6f7547f031a32e7fc005886fa562c1a8055c1976
Julian Garcia <jcgrrincon@gmail.com>
2022-12-09 17:35:14 -0500

    I mounted the test enviroment with jest and supertest

--- server/src/lib/db.ts
+++ server/src/lib/db.ts
@@ -8,17 +8,27 @@ class connection {
   private USERNAME:string | undefined;
   private PASSWORD:string|undefined;
   private DATA_BASES:string|undefined;
+  private URL:string|undefined;
+  private ENV:string;
 
   constructor(){
     this.USERNAME = process.env.MONGO_USERNAME;
     this.PASSWORD = process.env.MONGO_PASSWORD;
     this.DATA_BASES = process.env.MONGO_DATABASE;
+    this.ENV = process.env.NODE_ENV!;
+    if (this.ENV==='production') {
+      this.URL = `mongodb+srv://${this.USERNAME}:${this.PASSWORD}@${this.DATA_BASES}.i09hovr.mongodb.net/?retryWrites=true&w=majority`
+    }else if(this.ENV === 'testing'){
+      this.URL = 'mongodb://localhost:27017/urlShortCutTest'
+    }else {
+      this.URL = 'mongodb://localhost:27017/urlShortCut'
+    }
   }
 
   async Connection(){
     try {
       if (!this.db) {
-        this.db = await mongoose.connect(`mongodb+srv://${this.USERNAME}:${this.PASSWORD}@${this.DATA_BASES}.i09hovr.mongodb.net/?retryWrites=true&w=majority`);
+        this.db = await mongoose.connect(this.URL!);
         console.log(`database is connected with mongodb`);
         await mongoose.syncIndexes()
         return this.db
